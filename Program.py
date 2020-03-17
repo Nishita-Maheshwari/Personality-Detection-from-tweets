0c26212faf167713d5c440aade71721b08f1eb50
Nishita-Maheshwari <nishita.maheshwari55@gmail.com>
2020-03-17 12:16:34 +0530

    print (tweet.text.translate(non_bmp_map))    print tweet's text

--- Program.py
+++ Program.py
@@ -43,6 +43,7 @@ class SentimentAnalysis:
 
         for tweet in self.tweets:
             self.tweetText.append(self.cleanTweet(tweet.text).encode('utf-8'))
+            analysis = TextBlob(tweet.text)
 
 
 
