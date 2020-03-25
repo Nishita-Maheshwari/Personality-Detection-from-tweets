92c6f51e3a6ab6e6093aaf44d9015b82f66df333
Nishita-Maheshwari <nishita.maheshwari55@gmail.com>
2020-03-25 13:23:48 +0530

    function to calculate percentage

--- Program.py
+++ Program.py
@@ -105,6 +105,7 @@ class SentimentAnalysis:
                           searchTerm, NoOfTerms)
 
     def cleanTweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) | (\w +:\ / \ / \S +)", " ", tweet).split())
 
 
 
